745d57d43e9f7956e31a19cc77b078bb66aacda9
Andrey <p46.l0w@gmail.com>
2022-09-27 17:09:45 +0300

    Add accesebility test for broken images

--- tests/brokenimages.spec.ts
+++ tests/brokenimages.spec.ts
@@ -1,15 +1,15 @@
 import test, { expect } from "@playwright/test";
-import { BrokenImages } from "../pageobjects/brokenimagespage";
+import { BrokenImagesPage } from "../pageobjects/brokenimagespage";
 
 
-test.describe.only("", () => {
+test.describe("", () => {
     // TODO: add accessebility test 
-    test("", async ({page, request}) => {
-        const brokenImages: BrokenImages = new BrokenImages(page);
+    test("Should return status OK for images source call", async ({page, request}) => {
+        const brokenImagesPage: BrokenImagesPage = new BrokenImagesPage(page);
 
-        await brokenImages.goto();
+        await brokenImagesPage.goto();
 
-        const sources = await brokenImages.getImagesSource();
+        const sources = await brokenImagesPage.getImagesSource();
 
         for (const source of sources) {
             let response = await request.get(source);
@@ -17,7 +17,21 @@ test.describe.only("", () => {
              `Call to ${source} should return OK status`)
              .toBeTruthy();
         }
-    })
+    });
+
+    test("Accesebility. Images gould have alt attribute", async ( {page} ) => {
+        const brokenImagesPage: BrokenImagesPage = new BrokenImagesPage(page);
+
+        await brokenImagesPage.goto();
+
+        const images = await brokenImagesPage.getImagesElements();
+        
+        for (let i = 1; i <= await images.count(); i++) {
+            await expect.soft(images.nth(i),
+             `Image have to have alt attribute`)
+             .toHaveAttribute("alt", /.+/, {timeout: 50});
+        }
+    });
 
 
 
